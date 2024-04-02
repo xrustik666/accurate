f04bcc4f31402da39f33c308a6347370631ea66a
xrustik666 <kilorust@gmail.com>
2024-04-02 14:26:27 +0200

    commit before implementing separate page for arhive results

--- src/elements.js
+++ src/elements.js
@@ -5,11 +5,11 @@ const baseCoin = groups[1];
 
 const amount = document.querySelector('.amount');
 const result = document.querySelector('.result');
-const convertButton = document.querySelector('.convertButton');
+const convertButton = document.querySelector('.convert-button');
 
 const fromDate = document.querySelector('.from-date');
 const toDate = document.querySelector('.to-date');
-const archiveButton = document.querySelector('.archiveButton');
+const archiveButton = document.querySelector('.archive-button');
 const archiveRatesList = document.querySelector('.archive-rates-list');
 
 export { groups, targetCoin, baseCoin, amount, result, convertButton, fromDate, toDate, archiveButton, archiveRatesList };
